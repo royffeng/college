4ace40bad1e2d00f4a32fe73c97dd6b6c9350952
royffeng <43551087+royffeng@users.noreply.github.com>
2020-04-03 12:49:45 -0700

    Update Playlist.h

--- CS014/Week1/LAB1/Playlist.h
+++ CS014/Week1/LAB1/Playlist.h
@@ -1,26 +1,30 @@
-//This header file was coded by LePendu, provided through the slack workspace
+#ifndef Playlist_h
+#define Playlist_h
 
-#ifndef PLAYLIST_H
-#define PLAYLIST_H
-#include <string>
+#include <iostream>
 using namespace std;
+
 class PlaylistNode {
-public:
-    PlaylistNode();
-    PlaylistNode(string, string, string, int, PlaylistNode* nextLoc = 0);
-    void InsertAfter(PlaylistNode*);
-    void SetNext(PlaylistNode* nodePtr);
-    string GetID() const;
-    string GetSongName() const;
-    string GetArtistName() const;
-    int GetSongLength() const;
-    PlaylistNode* GetNext();
-    void PrintPlaylistNode();
+    
 private:
     string uniqueID;
     string songName;
     string artistName;
     int songLength;
     PlaylistNode* nextNodePtr;
+
+public:
+    PlaylistNode();
+    PlaylistNode(string, string, string, int);
+    void InsertAfter(PlaylistNode*);
+    void SetNext(PlaylistNode*);
+    string GetID() const;
+    string GetSongName() const;
+    string GetArtistName() const;
+    int GetSongLength() const;
+    PlaylistNode* GetNext() const;
+    void PrintPlaylistNode(PlaylistNode*) const;
 };
+
+
 #endif
