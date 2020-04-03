d9307e73154a42f8635e2b0441555a08a84de79e
royffeng <43551087+royffeng@users.noreply.github.com>
2020-04-03 12:48:54 -0700

    Update Playlist.cpp

--- CS014/Week1/LAB1/Playlist.cpp
+++ CS014/Week1/LAB1/Playlist.cpp
@@ -1,26 +1,23 @@
+//Header file was largely provided by Professor LePendu through slack
+#include <stdio.h>
 #include <iostream>
-#include <iomanip>
-#include "Playlist.h"
-
 using namespace std;
+
+#include "Playlist.h"
 
 PlaylistNode::PlaylistNode() : uniqueID("none"), songName("none"), artistName("none"), songLength(0), nextNodePtr(nullptr) {}
 
-PlaylistNode::PlaylistNode(string uniqueID, string songName, string artistName, int songLength) {
-    this->uniqueID = uniqueID;
-    this->songName = songName;
-    this->artistName = artistName;
-    this->songLength = songLength;
+PlaylistNode::PlaylistNode(string uniqueID, string songName, string artistName, int songLength) : uniqueID(uniqueID), songName(songName), artistName(artistName), songLength(songLength), nextNodePtr(nullptr) {}
+
+void PlaylistNode::InsertAfter(PlaylistNode* data) {
+    PlaylistNode* temp = nullptr;
+    temp = nextNodePtr;
+    nextNodePtr = data;
+    data->SetNext(temp);
 }
 
-void PlaylistNode::InsertAfter(PlaylistNode* node) {
-    PlaylistNode* temp = nextNodePtr;
-    nextNodePtr = node;
-    node->SetNext(temp);
-}
-
-void PlaylistNode::SetNext(PlaylistNode* node) {
-    nextNodePtr = node;
+void PlaylistNode::SetNext(PlaylistNode* next) {
+    nextNodePtr = next;
 }
 
 string PlaylistNode::GetID() const {
@@ -43,180 +40,9 @@ PlaylistNode* PlaylistNode::GetNext() const {
     return nextNodePtr;
 }
 
-void PlaylistNode::PrintPlaylistNode() const {
-    cout << "Unique ID: " << uniqueID << endl;
-    cout << "Song Name: " << songName << endl;
-    cout << "Artist Name: " << artistName << endl;
-    cout << "Song Length (in seconds): " << songLength << endl;
-}
-
-Playlist::Playlist() : head(nullptr), tail(nullptr) {}
-
-void Playlist::print(string title) const {
-    cout << title << " - OUTPUT FULL PLAYLIST\n";
-    if (empty()) {
-        cout << "Playlist is empty\n";
-    }
-    else {
-        int counter = 1;
-        PlaylistNode* node = head;
-        while (node) {
-            cout << counter << ".\n";
-            node->PrintPlaylistNode();
-
-            counter++;
-            node = node->GetNext();
-        }
-    }
-}
-
-bool Playlist::empty() const {
-    return head == nullptr;
-}
-
-void Playlist::push_back(PlaylistNode* node) {
-    if (empty()) {
-        head = node;
-        tail = node;
-    }
-    else {
-        tail->SetNext(node);
-        tail = node;
-    }
-}
-
-PlaylistNode* Playlist::remove(string id) {
-    PlaylistNode* prevPointer = nullptr;
-    PlaylistNode* currPointer = head;
-
-    while (currPointer && currPointer->GetID() != id) {
-        prevPointer = currPointer;
-        currPointer = currPointer->GetNext();
-    }
-
-    if (currPointer) {
-        if (prevPointer) {
-            prevPointer->SetNext(currPointer->GetNext());
-        }
-        else {
-            head = currPointer->GetNext();
-        }
-
-        if (!currPointer->GetNext()) {
-            tail = prevPointer;
-        }
-
-        return currPointer;
-    }
-    else {
-        return nullptr;
-    }
-}
-
-void Playlist::changePos(int initialPos, int newPos) {
-    int initialIndex = 1;
-    PlaylistNode* prev = nullptr;
-    PlaylistNode* curr = head;
-
-    if (initialPos < 1) {
-        cout << "Invalid current position.\n";
-        return;
-    }
-
-    //Find the songPosition
-    while (initialIndex < initialPos) {
-        if (curr) {
-            prev = curr;
-            curr = curr->GetNext();
-            initialIndex++;
-
-            if (!curr) {
-                cout << "Invalid current position.\n";
-                return;
-            }
-        }
-        else {
-            cout << "Invalid current position.\n";
-            return;
-        }
-    }
-
-    if (prev) {
-        prev->SetNext(curr->GetNext());
-    }
-    else {
-        head = curr->GetNext();
-    }
-
-    if (curr == tail) {
-        tail = prev;
-    }
-
-    if (newPos <= 1) {
-        if (empty()) {
-            tail = curr;
-        }
-
-        curr->SetNext(head);
-        head = curr;
-        cout << '"' << curr->GetSongName() << '"' << " moved to position 1\n";
-    }
-    else // Node is inserted after the head
-    {
-        if (empty()) // The list is empty after removing the node
-        {
-            push_back(curr);
-            cout << '"' << curr->GetSongName() << '"' << " moved to position 1" << endl;
-        }
-        else
-        {
-            int newPosIndex = 2;
-            PlaylistNode* prevNode = head;
-            PlaylistNode* afterNode = head->GetNext();
-            while (newPosIndex < newPos && afterNode)
-            {
-                prevNode = afterNode;
-                afterNode = afterNode->GetNext();
-                newPosIndex++;
-            }
-
-            prevNode->InsertAfter(curr);
-
-            if (!afterNode) // the node is being inserted at the tail
-            {
-                tail = curr;
-            }
-            cout << '"' << curr->GetSongName() << '"' << " moved to position " << newPosIndex << endl;
-        }
-    }
-}
-
-void Playlist::printByArtist(string artist) const
-{
-    int positionCounter = 1;
-    PlaylistNode* node = head;
-    while (node)
-    {
-        if (node->GetArtistName() == artist)
-        {
-            cout << positionCounter << "." << endl;
-            node->PrintPlaylistNode();
-            cout << endl;
-        }
-
-        positionCounter++;
-        node = node->GetNext();
-    }
-}
-
-int Playlist::totalTimeSeconds() const
-{
-    int total = 0;
-    PlaylistNode* node = head;
-    while (node)
-    {
-        total += node->GetSongLength();
-        node = node->GetNext();
-    }
-    return total;
+void PlaylistNode::PrintPlaylistNode(PlaylistNode* nextData) const {
+    cout << "Unique ID: " << nextData->GetID() << endl
+         << "Song Name: " << nextData->GetSongName() << endl
+         << "Artist Name: " << nextData->GetArtistName() << endl
+         << "Song Length (in seconds): " << nextData->GetSongLength() << endl << endl;
 }
